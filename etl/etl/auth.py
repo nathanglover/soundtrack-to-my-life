d8140e7353ad6fda91461b9328d8d8549c74cf19
Nathan Glover <nlglover9@gmail.com>
2020-03-22 21:00:20 -0400

    some fixes

--- etl/etl/auth.py
+++ etl/etl/auth.py
@@ -16,14 +16,14 @@ class SpotifyAPIAuth(requests.auth.AuthBase):
         self.token_url = "https://accounts.spotify.com/api/token"
 
     @staticmethod
-    def __get_client_secret_auth() -> (str, str):
+    def _get_client_secret_auth() -> (str, str):
         print("Getting client id and client secret")
         parameter_name = "spotify-api-client-secrets"
         response = client.get_parameter(Name=parameter_name, WithDecryption=True)
         data = json.loads(response["Parameter"]["Value"])
         return (data["client_id"], data["client_secret"])
 
-    def __set_tokens(self) -> dict:
+    def _set_tokens(self) -> dict:
         response = client.get_parameter(Name=self.parameter_name, WithDecryption=True)
         parameter = response["Parameter"]
         last_modifed = parameter["LastModifiedDate"]
@@ -37,7 +37,7 @@ class SpotifyAPIAuth(requests.auth.AuthBase):
         if now > expires_at:
             self.refresh_tokens()
 
-    def __save_new_tokens(self, tokens) -> dict:
+    def _save_new_tokens(self, tokens) -> dict:
         print("Saving new tokens to s3.")
         value = json.dumps(tokens)
         return client.put_parameter(
@@ -52,17 +52,17 @@ class SpotifyAPIAuth(requests.auth.AuthBase):
             "refresh_token": refresh_token,
         }
         res = requests.post(
-            self.token_url, data=data, auth=self.__get_client_secret_auth()
+            self.token_url, data=data, auth=self._get_client_secret_auth()
         )
         new_tokens = res.json()
         if "refresh_token" not in new_tokens:
             new_tokens["refresh_token"] = refresh_token
 
-        self.__save_new_tokens(new_tokens)
+        self._save_new_tokens(new_tokens)
         self.tokens = new_tokens
 
     def __call__(self, r):
         print("Authenticating with Spotify")
-        self.__set_tokens()
+        self._set_tokens()
         r.headers["authorization"] = f"Bearer {self.tokens['access_token']}"
         return r
--- etl/etl/tests/test_auth.py
+++ etl/etl/tests/test_auth.py
@@ -7,7 +7,3 @@ def test_init():
     auth = SpotifyAPIAuth()
     assert auth.parameter_name == "spotify-api-access-tokens"
     assert auth.token_url == "https://accounts.spotify.com/api/token"
-
-
-def test__get_client_secret_auth():
-    auth = SpotifyAPIAuth()
