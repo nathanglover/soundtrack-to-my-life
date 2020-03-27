7973cfdadbe7f583571fb58662bd80c1278c38ab
Nathan Glover <nlglover9@gmail.com>
2020-03-26 22:12:01 -0400

    cleaner cli.py

--- etl/cli.py
+++ etl/cli.py
@@ -1,37 +1,27 @@
-import sys
-
 import click
 import handlers
 
 
-def currently_playing(save=False):
-    handlers.currently_playing(None, None, cli=True, save=save)
-
-
-def insert_timeline_documents(save=False):
-    handlers.insert_timeline_documents(None, None, cli=True, save=save)
-
-
 @click.command()
 @click.option(
-    "--handler",
-    "-H",
-    prompt="Handler",
-    help="The etl handler to run, must be listed in handlers.py.",
+    "--function",
+    "-f",
+    prompt="function",
+    help="The etl function to run, must be listed in handlers.py.",
 )
 @click.option(
     "--save/--no-save",
-    "-S",
+    "-s",
     default=False,
     help="Save output of handler to s3 or database (where applicable).",
 )
-def handler(handler, save):
-    handler = getattr(sys.modules[__name__], handler, None)
+def handler(function, save):
+    handler = getattr(handlers, function, None)
     if not handler:
-        raise ValueError("Handler does not exist in cli.py.")
+        msg = f"{function} not found in handlers.py"
+        raise ModuleNotFoundError(msg)
 
-    if handler:
-        handler(save=save)
+    handler(None, None, cli=True, save=save)
 
 
 if __name__ == "__main__":
